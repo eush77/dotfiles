74a84a1eecbcb8e009eea660e27854075e0f0cd4
eush77 <eush77@gmail.com>
2013-11-07 01:23:06 +0400

    Begin with current session name when changing it

--- res+conf/fish/functions/fish_session.fish
+++ res+conf/fish/functions/fish_session.fish
@@ -2,6 +2,6 @@ function fish_session -a name --description='Name this shell session'
          if test $name
             set -g fish_session_name $name
          else
-            read fish_session_name
+            read --command=$fish_session_name fish_session_name
          end
 end
