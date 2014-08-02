82fab2347067a35df51afb6aa6540c9f31973c29
Eugene Sharygin <eush77@gmail.com>
2014-08-02 17:55:26 +0400

    Remove Fish sessions

--- res+conf/fish/functions/fish_greeting.fish
+++ res+conf/fish/functions/fish_greeting.fish
@@ -1,16 +1,2 @@
-set -gx fish_session_word_generator ~/.config/fish/functions/fish_greeting.d/word
-set -gx fish_greeting_color_session 89272b
-
 function fish_greeting --description 'Configured Fish greeting'
-         printf 'Welcome to Fish. '
-         if test $SHLVL -eq 1
-            set -gx fish_session_name (eval $fish_session_word_generator)
-            printf "Session name: "
-            set_color $fish_greeting_color_session
-            printf $fish_session_name
-            set_color normal
-            printf ".\nDon't hesitate to change it via fish_session.\n"
-         else
-            printf "This shell is $SHLVL shells deep.\n"
-         end
 end
--- res+conf/fish/functions/fish_prompt.fish
+++ res+conf/fish/functions/fish_prompt.fish
@@ -1,15 +1,12 @@
-set -g fish_prompt_color_session 4d1618
 set -g fish_prompt_color_path 423189
+set -g fish_prompt_color_equivalent_path brown
 
 function fish_prompt --description='Fancy prompt'
-         set_color $fish_prompt_color_session
-         if test $SHLVL -ne 1
-            printf '<%d> ' $SHLVL
+         if test $TMUX
+            set_color $fish_prompt_color_equivalent_path
+         else
+            set_color $fish_prompt_color_path
          end
-         printf $fish_session_name
-         set_color normal
-         printf ' '
-         set_color $fish_prompt_color_path
          printf (prompt_pwd)
          set_color normal
          printf ' ❩  '
