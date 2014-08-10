bdf40aa6fd5961ddbd2874ef03c3e40076ad8256
Eugene Sharygin <eush77@gmail.com>
2014-08-10 22:44:34 +0400

    Remove the remaining Fish sessions script

--- res+conf/fish/functions/fish_session.fish
+++
@@ -1,7 +0,0 @@
-function fish_session -a name --description='Name this shell session'
-         if test $name
-            set -g fish_session_name $name
-         else
-            read --command=$fish_session_name fish_session_name
-         end
-end
