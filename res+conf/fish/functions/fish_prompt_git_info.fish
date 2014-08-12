bad54d1c59a21d28bd5762dce80ee0fce3be7752
Eugene Sharygin <eush77@gmail.com>
2014-08-12 15:37:38 +0400

    Add Tmux alternative colors to git_info

--- res+conf/fish/functions/fish_prompt_git_info.fish
+++ res+conf/fish/functions/fish_prompt_git_info.fish
@@ -1,15 +1,28 @@
+set -g fish_prompt_git_info_clean green
+set -g fish_prompt_git_info_equivalent_clean green
+set -g fish_prompt_git_info_dirty yellow
+set -g fish_prompt_git_info_equivalent_dirty red
+
 function fish_prompt_git_info --argument-names text_color --description='Include Git info in the current prompt'
+         if test $TMUX
+            set clean_color $fish_prompt_git_info_equivalent_clean
+            set dirty_color $fish_prompt_git_info_equivalent_dirty
+         else
+            set clean_color $fish_prompt_git_info_clean
+            set dirty_color $fish_prompt_git_info_dirty
+         end
+
          if git status >/dev/null ^&1
             printf '['
 
             set branch (git rev-parse --abbrev-ref HEAD)
 
             if test (count (git status --short)) -eq 0
-               set_color green
+               set_color $clean_color
                printf '%s' $branch
             else
                # Dirty state.
-               set_color yellow
+               set_color $dirty_color
                printf '%s*' $branch
             end
 
