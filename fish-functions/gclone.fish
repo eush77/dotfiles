8cb0352ed71645d707bf3eb59e40992866ec215a
Eugene Sharygin <eush77@gmail.com>
2015-02-15 13:31:17 +0300

    (fish) Remove all the asynchronous stuff from gclone.fish

--- fish-functions/gclone.fish
+++ fish-functions/gclone.fish
@@ -1,23 +1,8 @@
-function gclone --description "git-clone + cd"
-  function _gclone_repo_address -a repo
-    if not printf "%s" $repo |grep :// >/dev/null
-      printf "https://github.com/%s" $repo
-    else
-      printf "%s" $repo
-    end
+function gclone -a repo --description "git-clone + cd"
+  if not printf "%s" $repo |grep :// >/dev/null
+    set repo (printf "https://github.com/%s" $repo)
   end
+  set -l dir (printf "%s" $repo |sed -r "s:.*/::;s:\.git\$::")
 
-  if test (count $argv) -eq 1
-    set argv[1] (_gclone_repo_address $argv[1])
-  end
-  functions -e _gclone_repo_address
-
-  set -l stdout (mktemp -u)
-  mkfifo $stdout
-
-  git clone $argv --progress 2>|tee $stdout &
-  set -l dir (cat $stdout |head -1 |grep -o "'.*'" |tr -d "'")
-  rm $stdout
-
-  cd $dir
+  git clone $repo; and cd $dir
 end
