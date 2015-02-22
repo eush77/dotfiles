19de4ad96a8847f3e7db707174409c1824bc41d2
Eugene Sharygin <eush77@gmail.com>
2015-02-22 04:35:53 +0300

    (fish/to) Resolve first path component through $CDPATH machinery

--- fish-functions/to.fish
+++ fish-functions/to.fish
@@ -1,4 +1,28 @@
-function to --argument-names dir --description='Make directory and move to it'
-  mkdir -p $dir
-  cd $dir
+function to -a dir --description="cd that can create things"
+  function _to_mkdir_cd -a path
+    mkdir -p $path
+    cd $path
+  end
+
+  # Handle $HOME and relative/absolute paths first.
+  # In these cases we can't safely unpack (and don't have to).
+  if test (count $argv) -eq 0
+    cd $argv
+  else if begin; printf "%s" $dir |grep '^[/.]' >/dev/null; end
+    _to_mkdir_cd $dir
+  else
+    set -l head (printf "%s" $dir |sed 's:/.*::')
+    set -l tail (printf "%s" $dir |sed 's:^[^/]*/*::')
+
+    # The first component might exist, in which case we cd straight there.
+    # This also resolves $head using $CDPATH.
+    # As an interesting corner case, -/whatever is resolved via $dirprev/$dirnext.
+    cd $head ^/dev/null; or _to_mkdir_cd $head
+
+    if test "$tail" != ""
+      _to_mkdir_cd $tail
+    end
+  end
+
+  functions -e _to_mkdir_cd
 end
