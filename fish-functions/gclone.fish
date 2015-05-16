9b77bcfca5733b69c317f31b1b9e8ae8a005e301
Eugene Sharygin <eush77@gmail.com>
2015-05-17 00:57:35 +0300

    (gclone) Add support for ssh synonyms

--- fish-functions/gclone.fish
+++ fish-functions/gclone.fish
@@ -1,8 +1,8 @@
 function gclone -a repo --description "git-clone + cd"
-  if not printf "%s" $repo |grep :// >/dev/null
+  if not printf "%s" $repo |grep -q :
     set repo (printf "https://github.com/%s" $repo)
   end
-  set -l dir (printf "%s" $repo |sed -r "s:.*/::;s:\.git\$::")
+  set -l dir (printf "%s" $repo |sed -r "s|.*[/:]||;s|\.git\$||")
 
   git clone $repo; and cd $dir
 end
