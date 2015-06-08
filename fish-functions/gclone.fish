cac8e2b8608bd2cb1d566c3e5e8f22e42dbc0663
Eugene Sharygin <eush77@gmail.com>
2015-06-08 14:19:59 +0300

    (gclone) Add --http flag

--- fish-functions/gclone.fish
+++ fish-functions/gclone.fish
@@ -1,6 +1,29 @@
-function gclone -a repo --description "git-clone + cd"
+function gclone --description "git-clone + cd"
+  eval set opt (getopt -o '' -l http -- $argv)
+
+  set -l use_http 0
+
+  while count $opt >/dev/null
+    switch $opt[1]
+      case --http
+        set use_http 1
+      case --
+        set -e opt[1]
+        break
+    end
+    set -e opt[1]
+  end
+
+  set -l repo $opt[1]
+
   if not printf "%s" $repo |grep -q :
-    set repo (printf "git@github.com:%s.git" $repo)
+    set -l repo_format
+    if test $use_http -eq 1
+      set repo_format "https://github.com/%s"
+    else
+      set repo_format "git@github.com:%s.git"
+    end
+    set repo (printf $repo_format $repo)
   end
   set -l dir (printf "%s" $repo |sed -r "s|.*[/:]||;s|\.git\$||")
 
