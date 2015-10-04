62f45b69c33d74f2311fc37ea6be466fe8fe8cfd
Eugene Sharygin <eush77@gmail.com>
2015-10-04 23:29:24 +0300

    fish: update config

--- res+conf/fish/config.fish
+++ res+conf/fish/config.fish
@@ -1,22 +1,18 @@
-set -gx CXX 'colorg++'
 set -gx EDITOR 'emacs -nw'
 set -gx PAGER 'less'
 set -gx LESS '--ignore-case --RAW-CONTROL-CHARS'
 set -gx LESSOPEN '| src-hilite-lesspipe.sh %s'
 set -gx MP '/mnt/mp'
-set -gx MP2 '/mnt/mp2'
 
 alias l 'ls -CF -BX --group-directories-first -v'
 alias la 'ls -alhFX --group-directories-first'
 alias lc 'ls -1 -BX -v --group-directories-first'
 
-#alias g git
-set -g fish_user_abbreviations "g=git"
+abbr -a g git
+abbr -a n npm
 
 alias emacs 'emacs -nw'
 alias matlab 'matlab -nodesktop -nosplash'
-alias re 'grep -P'
-alias se 'sed -r'
 
 set -gx LESS_TERMCAP_mb (printf '\e[01;31m')
 set -gx LESS_TERMCAP_md (printf '\e[01;31m')
