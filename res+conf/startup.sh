b84ae198718c0ad5bae5697f501d9866eb53f7d2
Eugene Sharygin <eush77@gmail.com>
2015-12-22 19:48:07 +0300

    startup: set 3 sec timeout for unclutter

--- res+conf/startup.sh
+++ res+conf/startup.sh
@@ -3,5 +3,5 @@
 # Set up keyboard layout.
 setxkbmap -rules evdev -model pc104 -layout "us,ru" -variant ",winkeys" -option "caps:ctrl_modifier,grp:lctrl_toggle,altwin:ctrl_win,grp_led:caps,compose:menu,numpad:microsoft"
 
-# Hide cursor after 1 sec of inactivity.
-unclutter -idle 1 &
+# Hide cursor after 3 sec of inactivity.
+unclutter -idle 3 &
