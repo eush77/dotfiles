ce49b15ffe9403ea370b0269a5b524455e96aba7
Eugene Sharygin <eush77@gmail.com>
2016-04-17 19:49:14 +0300

    fish: simplify `to` function
    Previously, this function was needlessly too complex, this commit makes
    it what it was intended to be: just a combination of `mkdir` and `cd`.

--- fish-functions/to.fish
+++ fish-functions/to.fish
@@ -1,28 +1,9 @@
-function to -a dir --description="cd that can create things"
-  function _to_mkdir_cd -a path
-    mkdir -p $path
-    cd $path
-  end
+function to -a dir --description="mkdir + cd"
+    mkdir -p $dir
 
-  # Handle $HOME and relative/absolute paths first.
-  # In these cases we can't safely unpack (and don't have to).
-  if test (count $argv) -eq 0
-    cd $argv
-  else if begin; printf "%s" $dir |grep '^[/.]' >/dev/null; end
-    _to_mkdir_cd $dir
-  else
-    set -l head (printf "%s" $dir |sed 's:/.*::')
-    set -l tail (printf "%s" $dir |sed 's:^[^/]*/*::')
-
-    # The first component might exist, in which case we cd straight there.
-    # This also resolves $head using $CDPATH.
-    # As an interesting corner case, -/whatever is resolved via $dirprev/$dirnext.
-    cd $head ^/dev/null; or _to_mkdir_cd $head
-
-    if test "$tail" != ""
-      _to_mkdir_cd $tail
+    if begin; printf "%s" $dir |grep '^[/.]' >/dev/null; end
+        cd $dir
+    else
+        cd ./$dir
     end
-  end
-
-  functions -e _to_mkdir_cd
 end
