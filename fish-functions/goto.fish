b81bd523909b81704b022a58269dfcd95ec16a75
Eugene Sharygin <eush77@gmail.com>
2016-08-21 22:22:03 +0300

    fish: update goto.fish to work with space characters

--- fish-functions/goto.fish
+++ fish-functions/goto.fish
@@ -1,4 +1,4 @@
 function goto --description "Print path to GO TO"
-  read --prompt="" --command=(pwd) --shell --local dir
-  builtin cd $dir
+    read --prompt="" --command=\"(pwd)\" --shell --local dir
+    eval builtin cd $dir
 end
