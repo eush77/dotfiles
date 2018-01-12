4277a590ee577d89e33ed2e7775b578b029d1983
Eugene Sharygin <eush77@gmail.com>
2018-01-12 10:41:55 +0300

    emacs,w3m: update W3M and Emacs-w3m configs
    - Update Emacs `browse-url' settings.
    - Set Emacs-w3m's `new-session' flags.
    - Make Emacs-w3m ignore WWW_HOME.
    - Bring key bindings closer together.

--- emacs/.emacs.d/config/config-w3m.el
+++ emacs/.emacs.d/config/config-w3m.el
@@ -1,10 +1,16 @@
+(custom-set browse-url-browser-function #'w3m-browse-url)
+(custom-set browse-url-generic-program nil)
+(custom-set browse-url-new-window-flag t)
+
 (with-eval-after-load "w3m"
   (custom-set w3m-fill-column 78)
   (custom-set w3m-default-display-inline-images t)
   (custom-set w3m-add-referer nil)
-  (custom-set browse-url-generic-program nil)
-  (custom-set w3m-make-new-session nil)
-  (custom-set w3m-new-session-in-background nil)
+  (custom-set w3m-make-new-session t)
+  (custom-set w3m-new-session-in-background t)
+  (custom-set w3m-home-page "about:")
+
+  (add-hook 'w3m-mode-hook #'w3m-lnum-mode)
 
   (add-hook 'w3m-mode-hook
             (lambda ()
@@ -23,4 +29,18 @@
       (when (listp (ad-get-arg 5))
         (ad-set-arg 5 (car (ad-get-arg 5))))))
 
-  (add-hook 'w3m-mode-hook #'w3m-lnum-mode))
+  (define-key w3m-mode-map (kbd "b") #'w3m-view-previous-page)
+  (define-key w3m-mode-map (kbd "B") #'w3m-view-next-page)
+  (define-key w3m-mode-map (kbd "M-c") #'w3m-cookie)
+  (define-key w3m-mode-map (kbd "f") #'w3m-lnum-follow)
+  (define-key w3m-mode-map (kbd "F") #'w3m-lnum-goto)
+  (define-key w3m-mode-map (kbd "h") #'w3m-history)
+  (define-key w3m-mode-map (kbd "j") #'scroll-up-line)
+  (define-key w3m-mode-map (kbd "k") #'scroll-down-line)
+  (define-key w3m-mode-map (kbd "l") #'recenter)
+  (define-key w3m-mode-map (kbd "m") #'w3m-lnum-external-view-this-url)
+  (define-key w3m-mode-map (kbd "M") #'w3m-external-view-current-url)
+  (define-key w3m-mode-map (kbd "n") #'w3m-next-anchor)
+  (define-key w3m-mode-map (kbd "C-c M-n") #'w3m-tab-move-right)
+  (define-key w3m-mode-map (kbd "p") #'w3m-previous-anchor)
+  (define-key w3m-mode-map (kbd "C-c M-p") #'w3m-tab-move-left))
