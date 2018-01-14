946e8faaa72bc1e937204008cc66b7de503f271f
Eugene Sharygin <eush77@gmail.com>
2018-01-14 20:30:07 +0300

    emacs: update view-mode-map

--- emacs/.emacs.d/config/config-view.el
+++ emacs/.emacs.d/config/config-view.el
@@ -1,3 +1,4 @@
 (with-eval-after-load "view"
   (define-key view-mode-map (kbd "j") #'scroll-up-line)
-  (define-key view-mode-map (kbd "k") #'scroll-down-line))
+  (define-key view-mode-map (kbd "k") #'scroll-down-line)
+  (define-key view-mode-map (kbd "l") #'recenter-top-bottom))
