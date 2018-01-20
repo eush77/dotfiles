9b4b2f02c504efc55530eb7d5a0c4ffc38ef910a
Eugene Sharygin <eush77@gmail.com>
2018-01-20 20:09:28 +0300

    emacs: define some View mode keybindings for Man mode

--- emacs/.emacs.d/config/config-man.el
+++ emacs/.emacs.d/config/config-man.el
@@ -1,3 +1,8 @@
 (with-eval-after-load "man"
   (custom-set Man-notify-method 'aggressive)
-  (custom-set Man-switches "-a"))
+  (custom-set Man-switches "-a")
+
+  (define-key Man-mode-map (kbd "j") #'scroll-up-line)
+  (define-key Man-mode-map (kbd "k") #'scroll-down-line)
+  (define-key Man-mode-map (kbd "l") #'recenter-top-bottom)
+  (define-key Man-mode-map (kbd "q") #'Man-kill))
