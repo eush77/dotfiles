6c21667c17e4dc6d510b91551b5e659005e71689
Eugene Sharygin <eush77@gmail.com>
2018-01-21 23:04:26 +0300

    emacs: update Org mode map

--- emacs/.emacs.d/config/config-org.el
+++ emacs/.emacs.d/config/config-org.el
@@ -1,4 +1,9 @@
 (with-eval-after-load "org"
+  (add-hook 'org-mode-hook #'auto-fill-mode)
+
+  (define-key org-mode-map (kbd "C-c j") #'org-promote-subtree)
+  (define-key org-mode-map (kbd "C-c k") #'org-demote-subtree)
+  (define-key org-mode-map (kbd "C-c n") #'org-move-subtree-down)
+  (define-key org-mode-map (kbd "C-c p") #'org-move-subtree-up)
   (define-key org-mode-map (kbd "C-c C-\\") #'org-toggle-link-display)
-  (define-key org-mode-map (kbd "C-c C-r") #'ivy-resume)
-  (add-hook 'org-mode-hook #'auto-fill-mode))
+  (define-key org-mode-map (kbd "C-c C-r") #'ivy-resume))
