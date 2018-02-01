e5420b2da6c5baf093aa5b39ab2d2661d2ee242c
Eugene Sharygin <eush77@gmail.com>
2018-02-01 11:26:00 +0300

    emacs: map show-next/show-previous commands in Org mode
    This commit ports the similar extension of Magit mode implemented in
    bd7c096 to Org.

--- emacs/.emacs.d/config/config-org.el
+++ emacs/.emacs.d/config/config-org.el
@@ -1,9 +1,37 @@
+;;; -*- lexical-binding: t -*-
+
 (with-eval-after-load "org"
   (add-hook 'org-mode-hook #'auto-fill-mode)
+
+  (let ((show-and-move
+         (lambda (move-next)
+           (outline-back-to-heading)
+           (outline-end-of-heading)
+           (when (not (outline-invisible-p))
+             (outline-hide-subtree)
+             (funcall move-next 1))
+           (outline-show-entry)
+           (outline-show-children)
+           (outline-back-to-heading))))
+
+    (defun my-outline-show-next-subtree ()
+      "If the subtree under the current heading is hidden, show it.
+Otherwise hide it, and show the next sibling subtree."
+      (interactive)
+      (funcall show-and-move #'outline-forward-same-level))
+
+    (defun my-outline-show-previous-subtree ()
+      "If the subtree under the current heading is hidden, show it.
+Otherwise hide it, and show the previous sibling subtree."
+      (interactive)
+      (funcall show-and-move #'outline-backward-same-level)))
+
+  (define-key org-mode-map (kbd "C-c C-r") #'ivy-resume)
 
   (define-key org-mode-map (kbd "C-c j") #'org-promote-subtree)
   (define-key org-mode-map (kbd "C-c k") #'org-demote-subtree)
   (define-key org-mode-map (kbd "C-c n") #'org-move-subtree-down)
   (define-key org-mode-map (kbd "C-c p") #'org-move-subtree-up)
   (define-key org-mode-map (kbd "C-c C-\\") #'org-toggle-link-display)
-  (define-key org-mode-map (kbd "C-c C-r") #'ivy-resume))
+  (define-key org-mode-map (kbd "M-N") #'my-outline-show-next-subtree)
+  (define-key org-mode-map (kbd "M-P") #'my-outline-show-previous-subtree))
