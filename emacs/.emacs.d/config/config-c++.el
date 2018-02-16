64fda8fe93fedbf4152f2a0fb1fd7881970fbe49
Eugene Sharygin <eush77@gmail.com>
2018-02-16 11:55:22 +0300

    emacs: update C++ config
    Set up indentation for namespace blocks and lambda arguments.

--- emacs/.emacs.d/config/config-c++.el
+++ emacs/.emacs.d/config/config-c++.el
@@ -1,1 +1,15 @@
-(c-set-offset 'topmost-intro '-)
+(with-eval-after-load "cc-mode"
+  (defun my-c++-lineup-lambda (langelem)
+    "Line up a C++ lambda argument."
+    (save-excursion
+      (back-to-indentation)
+      (backward-up-list)
+      (when (looking-back ")\s*\\(mutable\s*\\)?")
+        0)))
+
+  (defun my-c++-hook ()
+    "My hook for C++ mode."
+    (c-set-offset 'innamespace '-)
+    (c-set-offset 'arglist-cont-nonempty
+                  (list #'my-c++-lineup-lambda #'c-lineup-arglist)))
+  (add-hook 'c++-mode-hook #'my-c++-hook))
