be7ca209e65d863b23dfe6293f71ea8053917916
Eugene Sharygin <eush77@gmail.com>
2018-02-17 23:27:06 +0300

    emacs: add key binding to call browse-url from Dired

--- emacs/.emacs.d/config/config-dired.el
+++ emacs/.emacs.d/config/config-dired.el
@@ -5,11 +5,18 @@
   (custom-set dired-dwim-target t)
   (add-hook 'dired-after-readin-hook #'dired-hide-details-mode)
 
+  (defun my-dired-browse-file ()
+    "Browse a file with `browse-url'."
+    (interactive)
+    (let ((file (dired-get-file-for-visit)))
+      (browse-url file)))
+
   (define-key dired-mode-map (kbd "C-M-p") #'window-jump-up)
   (define-key dired-mode-map (kbd "C-M-n") #'window-jump-down)
 
   (define-key dired-mode-map (kbd "SPC") #'dired-up-directory)
   (define-key dired-mode-map (kbd "C-S-p") #'dired-prev-subdir)
   (define-key dired-mode-map (kbd "C-S-n") #'dired-next-subdir)
+  (define-key dired-mode-map (kbd "b") #'my-dired-browse-file)
   (define-key dired-mode-map (kbd "c") #'dired-kill-subdir)
   (define-key dired-mode-map (kbd "z") #'dired-hide-subdir))
