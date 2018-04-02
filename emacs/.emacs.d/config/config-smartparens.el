81c8739bf729929b3138422be8e829a428a26b7c
Eugene Sharygin <eush77@gmail.com>
2018-04-02 23:17:47 +0300

    emacs/smartparens: improve support for angle brackets in C++
    Don't consider an angle bracket to be paired when it is part of an
    arrow, comparision, or a shift operator. Also check if it's used in a
    template declarator or an include directive. Disable `navigate' action
    for angle brackets, so that they are allowed to be unbalanced.

--- emacs/.emacs.d/config/config-smartparens.el
+++ emacs/.emacs.d/config/config-smartparens.el
@@ -11,7 +11,28 @@ See `sp-wrap-with-pair', ‘sp-select-next-thing’."
     (interactive "P")
     (sp-wrap-with-pair "("))
 
+  (defun my-sp-c++-point-at-arrow-operator-p (id action context)
+    "True if angle bracket is part of the arrow operator."
+    (looking-back "->" nil))
+
+  (defun my-sp-c++-point-at-comparison-operator-p (id action context)
+    "True if angle bracket is part of the comparison operator."
+    (and (string= id "<")
+         (looking-back " ." nil)
+         (not (looking-back "\\(template\\|#include\\) <" nil))))
+
+  (defun my-sp-c++-point-at-shift-operator-p (id action context)
+    "True if angle bracket is part of the comparison operator."
+    (looking-back "<<\\|>>" nil))
+
   ;; Use asymmetric single quotes.
   (sp-pair "`" "'")
 
-  (sp-local-pair 'c++-mode "<" ">"))
+  ;; C++ angle brackets are overloaded for different things. Disable
+  ;; strictness checks (by not listing the `navigate' action) and add some
+  ;; filters.
+  (sp-local-pair 'c++-mode "<" ">"
+                 :actions '(insert wrap autoskip)
+                 :unless '(my-sp-c++-point-at-arrow-operator-p
+                           my-sp-c++-point-at-comparison-operator-p
+                           my-sp-c++-point-at-shift-operator-p)))
