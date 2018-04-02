96e6e6ccce0e4ea384febcc8d55bb152e6fd9caa
Eugene Sharygin <eush77@gmail.com>
2018-04-02 11:57:24 +0300

    emacs/smartparens: add angle brackets for C++ mode

--- emacs/.emacs.d/config/config-smartparens.el
+++ emacs/.emacs.d/config/config-smartparens.el
@@ -4,12 +4,14 @@
   (require 'smartparens-config)
   (custom-set sp-highlight-pair-overlay nil)
 
-  ;; Use asymmetric single quotes.
-  (sp-pair "`" "'")
-
   (defun my-sp-wrap-with-pair (&optional arg)
     "Wrap the following expression in parentheses.
 
 See `sp-wrap-with-pair', ‘sp-select-next-thing’."
     (interactive "P")
-    (sp-wrap-with-pair "(")))
+    (sp-wrap-with-pair "("))
+
+  ;; Use asymmetric single quotes.
+  (sp-pair "`" "'")
+
+  (sp-local-pair 'c++-mode "<" ">"))
