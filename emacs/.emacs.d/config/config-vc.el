4525179363d07315ab91620daf7da7d61615f8d5
Eugene Sharygin <eush77@gmail.com>
2018-04-03 20:14:40 +0300

    emacs/vc: add command to visit commit at point in Magit

--- emacs/.emacs.d/config/config-vc.el
+++ emacs/.emacs.d/config/config-vc.el
@@ -1,2 +1,10 @@
 (with-eval-after-load "vc"
   (custom-set vc-follow-symlinks t))
+
+(with-eval-after-load "vc-annotate"
+  (defun my-vc-annotate-magit-show-commit ()
+    "Visit commit at line in Magit."
+    (interactive)
+    (magit-show-commit (car (split-string (thing-at-point 'line) " "))))
+
+  (define-key vc-annotate-mode-map (kbd "m") #'my-vc-annotate-magit-show-commit))
