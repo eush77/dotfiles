ee15ecd20c55a75a5c7ddb079e0a20d4de9a5b16
Eugene Sharygin <eush77@gmail.com>
2018-04-06 00:06:19 +0300

    emacs/markdown,smartparens: set up inline-code pair
    This overrides the default global 'asymmetric single quotes' pair that
    is invalid in Markdown syntax.

--- emacs/.emacs.d/config/config-markdown.el
+++ emacs/.emacs.d/config/config-markdown.el
@@ -3,4 +3,7 @@
   (define-key markdown-mode-map (kbd "M-<right>") #'markdown-demote)
   (define-key markdown-mode-map (kbd "M-<up>") #'markdown-move-up)
   (define-key markdown-mode-map (kbd "M-<down>") #'markdown-move-down)
-  (add-hook 'markdown-mode-hook #'auto-fill-mode))
+  (add-hook 'markdown-mode-hook #'auto-fill-mode)
+
+  (with-eval-after-load "smartparens"
+    (sp-local-pair 'markdown-mode "`" "`")))
