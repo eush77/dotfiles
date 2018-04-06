3f26142ddf65044dadc4e6f7df4da9801a307b45
Eugene Sharygin <eush77@gmail.com>
2018-04-06 12:11:10 +0300

    emacs/c++,smartparens: assign `C-c C-u' key bindings to barf-sexp commands
    By default `C-c C-u' is bound to `c-up-conditional' which is not as
    useful.

--- emacs/.emacs.d/config/config-c++.el
+++ emacs/.emacs.d/config/config-c++.el
@@ -18,9 +18,11 @@
 
   (define-key c++-mode-map (kbd "C-c C-a") #'sp-beginning-of-sexp)
   (define-key c++-mode-map (kbd "C-c C-b") #'sp-backward-sexp)
+  (define-key c++-mode-map (kbd "C-c C-d") #'sp-kill-sexp)
   (define-key c++-mode-map (kbd "C-c C-e") #'sp-end-of-sexp)
   (define-key c++-mode-map (kbd "C-c C-f") #'sp-forward-sexp)
-  (define-key c++-mode-map (kbd "C-c C-d") #'sp-kill-sexp)
+  (define-key c++-mode-map (kbd "C-c C-u C-M-b") #'sp-backward-barf-sexp)
+  (define-key c++-mode-map (kbd "C-c C-u C-M-f") #'sp-forward-barf-sexp)
   (define-key c++-mode-map [remap indent-region] #'clang-format-region)
 
   (with-eval-after-load "smartparens"
