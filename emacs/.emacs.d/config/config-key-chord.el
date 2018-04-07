0f1ff05ccb98e21cbca84ae13c8079f7abef0d1e
Eugene Sharygin <eush77@gmail.com>
2018-04-07 10:19:49 +0300

    emacs/key-chord: add chord for quit-window

--- emacs/.emacs.d/config/config-key-chord.el
+++ emacs/.emacs.d/config/config-key-chord.el
@@ -6,6 +6,7 @@
 (key-chord-define-global "x0" #'my-balanced-delete-window)
 (key-chord-define-global "xb" #'ivy-switch-buffer)
 (key-chord-define-global "xk" #'kill-buffer)
+(key-chord-define-global "xq" #'quit-window)
 (key-chord-define-global "xs" #'save-buffer)
 (key-chord-define-global "xf" #'counsel-find-file)
 (key-chord-define-global "xl" #'counsel-find-library)
