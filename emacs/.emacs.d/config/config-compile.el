0f21631848b654cfd751d66e090e9b2c48ac7af2
Eugene Sharygin <eush77@gmail.com>
2018-04-13 17:57:48 +0300

    emacs/compile: don't save buffers in non-prog-mode-derived modes

--- emacs/.emacs.d/config/config-compile.el
+++ emacs/.emacs.d/config/config-compile.el
@@ -1,6 +1,13 @@
 (custom-set compilation-auto-jump-to-first-error t)
 (custom-set compilation-scroll-output t)
 
+(defun my-compilation-save-buffers-predicate ()
+  "Limit buffers that are saved before compiling to those derived
+from `prog-mode'."
+  (derived-mode-p 'prog-mode))
+(custom-set compilation-save-buffers-predicate
+            #'my-compilation-save-buffers-predicate)
+
 ;;;###autoload
 (defun my-recompile ()
   "Save current buffer, recompile, then switch to the compilation
