c39e370eedf50857e6a8d4878fb95bcc89d63289
Eugene Sharygin <eush77@gmail.com>
2018-04-14 13:13:18 +0300

    emacs/emms: add

---
+++ emacs/.emacs.d/config/config-emms.el
@@ -0,0 +1,19 @@
+(emms-all)
+(emms-default-players)
+
+(with-eval-after-load "emms-volume-pulse"
+  (defun my-emms-pulse-get-sink ()
+    "Get active PulseAudio sink.
+
+Active sink is defined as the last one in the list printed by
+`pactl'."
+    (with-temp-buffer
+      (call-process "pactl" nil t nil "list" "short" "sinks")
+      (cadr (split-string (buffer-substring (line-beginning-position 0)
+                                            (point-max))))))
+
+  (defun my-emms-volume-pulse-get-volume--select-sink ()
+    "Select the active `emms-volume-pulse-sink'."
+    (setq emms-volume-pulse-sink (my-emms-pulse-get-sink)))
+  (advice-add 'emms-volume--pulse-get-volume
+              :before #'my-emms-volume-pulse-get-volume--select-sink))
