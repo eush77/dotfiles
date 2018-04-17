de830ce9a5b33b05a6f8eb1439ae66a387641782
Eugene Sharygin <eush77@gmail.com>
2018-04-17 17:40:25 +0300

    emacs/gdb-mi: use stack buffer as a control buffer
    Define Gdb execution commands in `gdb-frames-mode-map' for using stack
    buffer as a control buffer.

--- emacs/.emacs.d/config/config-gdb-mi.el
+++ emacs/.emacs.d/config/config-gdb-mi.el
@@ -8,11 +8,16 @@ won't use it for source buffers."
             :after #'my-gdb-setup-windows--dedicate-comint-window)
 
 (defun my-gdb-eval-in-comint-buffer ()
-  "Switch to Gdb Comint buffer and populate the input line for
-evaluating an expression."
+  "Switch to Gdb Comint buffer and clear the input line."
   (interactive)
   (switch-to-buffer gud-comint-buffer)
-  (comint-delete-input)
+  (comint-delete-input))
+
+(defun my-gdb-print-in-comint-buffer ()
+  "Switch to Gdb Comint buffer and populate the input line for
+printing an expression."
+  (interactive)
+  (my-gdb-eval-in-comint-buffer)
   (insert "p "))
 
 (defun my-gdb-select-relative-frame (offset)
@@ -22,13 +27,13 @@ current frame."
   (forward-line offset)
   (gdb-select-frame))
 
-(defun my-gdb-select-next-frame ()
-  "Select next frame in Gdb stack buffer."
+(defun my-gdb-frame-up ()
+  "Up one frame in Gdb stack buffer."
   (interactive)
   (my-gdb-select-relative-frame 1))
 
-(defun my-gdb-select-previous-frame ()
-  "Select previous frame in Gdb stack buffer."
+(defun my-gdb-frame-down ()
+  "Down one frame in Gdb stack buffer."
   (interactive)
   (my-gdb-select-relative-frame -1))
 
@@ -37,8 +42,37 @@ current frame."
   (interactive)
   (switch-to-buffer (gdb-get-buffer 'gdb-stack-buffer)))
 
+(defun my-gdb-mode-hook ()
+  "My hook for GDB-MI mode."
+  (gud-def my-gud-reverse-cont
+           (gdb-gud-context-call "-exec-continue --reverse")
+           nil
+           "Continue backwards with display.")
+  (gud-def my-gud-reverse-finish
+           (gdb-gud-context-call "-exec-finish --reverse" nil t)
+           nil
+           "Finish executing current function backwards.")
+  (gud-def my-gud-reverse-next
+           (gdb-gud-context-call "-exec-next --reverse" "%p" t)
+           nil
+           "Step one line backwards (skip functions).")
+  (gud-def my-gud-reverse-step
+           (gdb-gud-context-call "-exec-step --reverse" "%p" t)
+           nil
+           "Step one source line backwards with display."))
+(add-hook 'gdb-mode-hook #'my-gdb-mode-hook)
+
+(define-key gdb-frames-mode-map (kbd "c") #'gud-cont)
+(define-key gdb-frames-mode-map (kbd "C") #'my-gud-reverse-cont)
+(define-key gdb-frames-mode-map (kbd "d") #'my-gdb-frame-down)
 (define-key gdb-frames-mode-map (kbd "e") #'my-gdb-eval-in-comint-buffer)
-(define-key gdb-frames-mode-map (kbd "n") #'my-gdb-select-next-frame)
-(define-key gdb-frames-mode-map (kbd "p") #'my-gdb-select-previous-frame)
+(define-key gdb-frames-mode-map (kbd "f") #'gud-finish)
+(define-key gdb-frames-mode-map (kbd "F") #'my-gud-reverse-finish)
+(define-key gdb-frames-mode-map (kbd "n") #'gud-next)
+(define-key gdb-frames-mode-map (kbd "N") #'my-gud-reverse-next)
+(define-key gdb-frames-mode-map (kbd "p") #'my-gdb-print-in-comint-buffer)
+(define-key gdb-frames-mode-map (kbd "s") #'gud-step)
+(define-key gdb-frames-mode-map (kbd "S") #'my-gud-reverse-step)
+(define-key gdb-frames-mode-map (kbd "u") #'my-gdb-frame-up)
 
 (define-key gud-mode-map (kbd "C-c s") #'my-gdb-switch-to-stack-buffer)
