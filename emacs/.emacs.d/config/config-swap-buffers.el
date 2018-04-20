85abbafc92d49fcb7f6037b96acb1c9c5f5c9a81
Eugene Sharygin <eush77@gmail.com>
2018-04-21 00:46:59 +0300

    emacs/swap-buffers: use ASCII-art labels in terminal
    Use labels defined in the `switch-window' package.

--- emacs/.emacs.d/config/config-swap-buffers.el
+++ emacs/.emacs.d/config/config-swap-buffers.el
@@ -1,1 +1,17 @@
 (custom-set swap-buffers-keep-focus t)
+
+(unless window-system
+  (require 'switch-window-asciiart)
+
+  (custom-set swap-buffers-qwerty-shortcuts
+              (mapcar (lambda (label)
+                        (let ((pos (1+ (string-match "(.)" label))))
+                          (substring label pos (1+ pos))))
+                      (butlast switch-window-asciiart)))
+
+  (defun my-swap-buffers-label--asciiart (num)
+    "My replacement for `swap-buffers-label' using
+`switch-window-asciiart'."
+    (nth (- num 1) switch-window-asciiart))
+  (advice-add 'swap-buffers-label
+              :override #'my-swap-buffers-label--asciiart))
