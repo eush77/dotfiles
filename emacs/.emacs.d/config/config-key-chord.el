84c2c1cd5888f4901d5905b088dc5c4beece23c9
Eugene Sharygin <eush77@gmail.com>
2018-04-20 19:41:13 +0300

    emacs/key-chord: order chord definitions

--- emacs/.emacs.d/config/config-key-chord.el
+++ emacs/.emacs.d/config/config-key-chord.el
@@ -1,15 +1,20 @@
 (key-chord-mode 1)
 
+;; Buffer operations
+(key-chord-define-global "xb" #'counsel-ibuffer)
+(key-chord-define-global "xk" #'kill-buffer)
+(key-chord-define-global "xs" #'save-buffer)
+
+;; Creating or deleting windows
+(key-chord-define-global "x0" #'my-balanced-delete-window)
 (key-chord-define-global "x1" #'delete-other-windows)
 (key-chord-define-global "x2" #'my-balanced-split-window-vertically)
 (key-chord-define-global "x3" #'my-balanced-split-window-horizontally)
-(key-chord-define-global "x0" #'my-balanced-delete-window)
-(key-chord-define-global "xb" #'counsel-ibuffer)
-(key-chord-define-global "xk" #'kill-buffer)
 (key-chord-define-global "xq" #'quit-window)
-(key-chord-define-global "xs" #'save-buffer)
+
+;; Finding files
 (key-chord-define-global "xf" #'find-file)
-(key-chord-define-global "xr" #'counsel-recentf)
-(key-chord-define-global "xl" #'find-library)
 (key-chord-define-global "xg" #'counsel-git)
+(key-chord-define-global "xl" #'find-library)
+(key-chord-define-global "xr" #'counsel-recentf)
 (key-chord-define-global "xv" #'find-alternate-file)
