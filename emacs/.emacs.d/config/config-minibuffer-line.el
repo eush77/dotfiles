0930c1a8740e9a0601e891b9512e291386e0fc26
Eugene Sharygin <eush77@gmail.com>
2018-04-26 09:22:30 +0300

    emacs/minibuffer-line: use fuzzy clock

--- emacs/.emacs.d/config/config-minibuffer-line.el
+++ emacs/.emacs.d/config/config-minibuffer-line.el
@@ -1,7 +1,19 @@
 (minibuffer-line-mode 1)
 
+(defun my-minibuffer-clock ()
+  "Formats the current time to include in the minibuffer line.
+
+Uses `bash-fuzzy-clock' [1,2] if it's installed, or else defaults
+to exact time.
+
+\[1]: URL `https://sourceforge.net/projects/bashfuzzyclock'
+\[2]: URL `https://aur.archlinux.org/packages/bash-fuzzy-clock'"
+  (condition-case nil
+      (string-trim-right (car (process-lines "bash-fuzzy-clock")))
+    (file-error (format-time-string "%R"))))
+
 (custom-set minibuffer-line-format
-            '(:eval (let ((time-string (format-time-string "%R")))
+            '(:eval (let ((time-string (my-minibuffer-clock)))
                       (concat (make-string (- (frame-text-cols)
                                               (string-width time-string)
                                               1)
