289bb2bb5db270f2ab21203e0a1b7b6e6446b079
Eugene Sharygin <eush77@gmail.com>
2018-04-28 19:38:52 +0300

    emacs/c: override Smartparens keys in c-mode-map

--- emacs/.emacs.d/config/config-c.el
+++ emacs/.emacs.d/config/config-c.el
@@ -3,3 +3,12 @@
   (c-set-offset 'arglist-intro '+)
   (c-set-offset 'inextern-lang 0))
 (add-hook 'c-mode-hook #'my-c-hook)
+
+(define-key c-mode-map (kbd "C-c C-a") #'sp-beginning-of-sexp)
+(define-key c-mode-map (kbd "C-c C-b") #'sp-backward-sexp)
+(define-key c-mode-map (kbd "C-c C-d") #'sp-kill-sexp)
+(define-key c-mode-map (kbd "C-c C-e") #'sp-end-of-sexp)
+(define-key c-mode-map (kbd "C-c C-f") #'sp-forward-sexp)
+(define-key c-mode-map (kbd "C-c C-u C-M-b") #'sp-backward-barf-sexp)
+(define-key c-mode-map (kbd "C-c C-u C-M-f") #'sp-forward-barf-sexp)
+(define-key c-mode-map [remap indent-region] #'clang-format-region)
