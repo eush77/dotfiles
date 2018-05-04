261f46335c11246089d5e3e1f67c99966c8f3997
Eugene Sharygin <eush77@gmail.com>
2018-05-04 10:19:38 +0400

    emacs/minibuffer-line: show exact time in a tooltip

--- emacs/.emacs.d/config/config-minibuffer-line.el
+++ emacs/.emacs.d/config/config-minibuffer-line.el
@@ -13,15 +13,19 @@ to exact time.
     (file-error (format-time-string "%R"))))
 
 (custom-set minibuffer-line-format
-            '(:eval (let ((time-string (my-minibuffer-clock))
+            '(:eval (let ((fuzzy-time-string (my-minibuffer-clock))
+                          (exact-time-string (format-time-string "%c"))
                           (min-frame-text-cols
                            (apply #'min
                                   (mapcar #'frame-text-cols
                                           (minibuffer-frame-list)))))
-                      (concat (make-string (- min-frame-text-cols
-                                              (string-width time-string))
+                      (propertize (concat (make-string
+                                           (- min-frame-text-cols
+                                              (string-width fuzzy-time-string))
                                            ? )
-                              time-string))))
+                                          fuzzy-time-string)
+                                  'help-echo exact-time-string
+                                  'mouse-face 'highlight))))
 
 (defun keyboard-quit--minibuffer-line (func)
   "Restore minibuffer line after quit."
