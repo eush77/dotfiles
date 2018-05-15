9d8cdc7edaf8bb2a29a1eafb6a65332dca879e18
Eugene Sharygin <eush77@gmail.com>
2018-05-15 08:11:40 +0300

    emacs/multitran: add config

---
+++ emacs/.emacs.d/config/config-multitran.el
@@ -0,0 +1,2 @@
+(define-key multitran-mode-map (kbd "l") #'multitran-history-prev)
+(define-key multitran-mode-map (kbd "r") #'multitran-history-next)
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -123,6 +123,7 @@ and URL
 (with-eval-after-load "info" (load "config-info"))
 (with-eval-after-load "man" (load "config-man"))
 (with-eval-after-load "markdown-mode" (load "config-markdown"))
+(with-eval-after-load "multitran" (load "config-multitran"))
 (with-eval-after-load "org" (load "config-org"))
 (with-eval-after-load "pocket-reader" (load "config-pocket-reader"))
 (with-eval-after-load "scroll-all" (load "config-scroll-all"))
