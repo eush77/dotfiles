e1ed54551d6153ab44a277bd935cd60f4d8fd8a4
Eugene Sharygin <eush77@gmail.com>
2018-05-21 11:56:45 +0300

    emacs/multitran: add missing autoload cookie

--- emacs/.emacs.d/config/config-multitran.el
+++ emacs/.emacs.d/config/config-multitran.el
@@ -1,6 +1,9 @@
+(require 'multitran)
+
 (define-key multitran-mode-map (kbd "l") #'multitran-history-prev)
 (define-key multitran-mode-map (kbd "r") #'multitran-history-next)
 
+;;;###autoload
 (defun my-multitran-region (select-langs)
   "Like `multitran', but ask for the word only if the region
 isn't active, otherwise use the region."
