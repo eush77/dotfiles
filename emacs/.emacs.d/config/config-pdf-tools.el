9a8e9a539952c044c9d628796d6790ce20081beb
Eugene Sharygin <eush77@gmail.com>
2018-05-24 15:00:54 +0300

    emacs/pdf-tools: bind isearch-backward in pdf-view-mode-map

--- emacs/.emacs.d/config/config-pdf-tools.el
+++ emacs/.emacs.d/config/config-pdf-tools.el
@@ -4,6 +4,7 @@
   (add-hook 'pdf-view-mode-hook #'pdf-view-midnight-minor-mode)
 
   ;; `pdf-isearch-minor-mode' integrates `pdf-tools' with `isearch'.
+  (define-key pdf-view-mode-map (kbd "C-r") #'isearch-backward)
   (define-key pdf-view-mode-map (kbd "C-s") #'isearch-forward)
 
   (define-key pdf-view-mode-map (kbd "j") #'pdf-view-next-line-or-next-page)
