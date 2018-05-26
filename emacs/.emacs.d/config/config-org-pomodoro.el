bc474e1a6044af9bf674ea242a2a791cbeb0391d
Eugene Sharygin <eush77@gmail.com>
2018-05-26 19:46:07 +0300

    emacs/org-pomodoro: don't clock back breaks if called with C-u C-u

--- emacs/.emacs.d/config/config-org-pomodoro.el
+++ emacs/.emacs.d/config/config-org-pomodoro.el
@@ -1,3 +1,19 @@
+(defun my-org-pomodoro--clock-in-last (func &rest args)
+  "Don't clock back break time if called with two prefix arguments,
+unless `org-pomodoro-clock-break' is t."
+  (cl-letf ((clock-in-last-function (symbol-function 'org-clock-in-last))
+            ((symbol-function 'org-clock-in-last)
+             (lambda (arg)
+               (interactive "P")
+               (funcall clock-in-last-function
+                        (if (and (equal arg '(16))
+                                 (not org-pomodoro-clock-break))
+                            0
+                          arg)))))
+    (apply func args)))
+(advice-add 'org-pomodoro
+            :around #'my-org-pomodoro--clock-in-last)
+
 (defun my-org-pomodoro-start--adjust-mode-line (func &rest args)
   "My adjustments to the `global-mode-string' when `org-pomodoro'
 is running."
