2bc3aa9d6f4f9bb5d25e1e3dad04964ec5103eca
Eugene Sharygin <eush77@gmail.com>
2018-06-10 11:18:48 +0300

    emacs/hideshow: disable while Outline minor mode is enabled
    There are (seemingly intentional) key binding conflicts between Hideshow
    and Outline minor mode, so since the former doesn't seem useful while
    the latter is enabled, disable it temporarily.

--- emacs/.emacs.d/config/config-hideshow.el
+++ emacs/.emacs.d/config/config-hideshow.el
@@ -1,4 +1,11 @@
-(dolist (hook '(emacs-lisp-mode-hook
-                c-mode-hook
-                c++-mode-hook))
-  (add-hook hook (lambda () (hs-minor-mode t))))
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+
+(defun my-outline-minor-mode--hideshow (&rest args)
+  "Disable `hs-minor-mode' while `outline-minor-mode' is
+enabled."
+  (if outline-minor-mode
+      (when (derived-mode-p 'prog-mode)
+        (hs-minor-mode 1))
+    (hs-minor-mode 0)))
+(advice-add 'outline-minor-mode
+            :before #'my-outline-minor-mode--hideshow)
