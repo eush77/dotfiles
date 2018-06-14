e96003bd6549af7c40ebe3720b7edf4d814b9030
Eugene Sharygin <eush77@gmail.com>
2018-06-14 23:15:38 +0300

    emacs/man: set notify method to pushy when calling Man from a Man buffer
    Avoid creating a new window for a new Man page.

--- emacs/.emacs.d/config/config-man.el
+++ emacs/.emacs.d/config/config-man.el
@@ -1,7 +1,18 @@
 (custom-set Man-notify-method 'aggressive)
 (custom-set Man-switches "-a")
 
+;;; Commands
+
+(defun my-man-pushy ()
+  "Like `man', but with `Man-notify-method' set to `pushy'."
+  (interactive)
+  (let ((Man-notify-method 'pushy))
+    (call-interactively #'man)))
+
+;;; Keymap
+
 (define-key Man-mode-map (kbd "j") #'scroll-up-line)
 (define-key Man-mode-map (kbd "k") #'scroll-down-line)
 (define-key Man-mode-map (kbd "l") #'recenter-top-bottom)
+(define-key Man-mode-map (kbd "m") #'my-man-pushy)
 (define-key Man-mode-map (kbd "q") #'Man-kill)
