09a72195686c7b460ce222a574a19f59105e14a8
Eugene Sharygin <eush77@gmail.com>
2018-08-06 17:30:26 +0300

    emacs/flycheck: add hydra

--- emacs/.emacs.d/config/config-flycheck.el
+++ emacs/.emacs.d/config/config-flycheck.el
@@ -2,3 +2,15 @@
 (custom-set flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
 (global-flycheck-mode 1)
+
+;;;###autoload (autoload 'my-flycheck-hydra/body "config-flycheck")
+(eval `(defhydra my-flycheck-hydra ()
+         "Flycheck"
+         ("p" flycheck-previous-error "previous error")
+         ("n" flycheck-next-error "next error")
+         (,(if window-system "<return>" "RET") nil)))
+
+(define-key flycheck-mode-map [remap flycheck-previous-error]
+  #'my-flycheck-hydra/flycheck-previous-error)
+(define-key flycheck-mode-map [remap flycheck-next-error]
+  #'my-flycheck-hydra/flycheck-next-error)
