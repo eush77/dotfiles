35d46d43340ce776078e2a1b723439dc60787a94
Eugene Sharygin <eush77@gmail.com>
2018-09-27 08:41:37 +0300

    emacs: fix my-frame-wm-desktop for remote buffers
    `my-frame-wm-desktop' previously executed `xprop' from a buffer's
    default directory, which does not work if a buffer is visiting a remote
    file (in which case the `xprop' program is either nonexistent or
    unrelated to the `xprop' on the local host). This commit switches to a
    local directory first.

--- emacs/.emacs.d/config/config-utils.el
+++ emacs/.emacs.d/config/config-utils.el
@@ -382,10 +382,12 @@ system, or nil."
     (string-to-number
      (cadr
       (split-string
-       (shell-command-to-string
-        (format "xprop -id %s _NET_WM_DESKTOP"
-                (alist-get 'outer-window-id
-                           (frame-parameters frame))))
+       ;; Run `xprop' from a local directory.
+       (let ((default-directory "~"))
+         (shell-command-to-string
+          (format "xprop -id %s _NET_WM_DESKTOP"
+                  (alist-get 'outer-window-id
+                             (frame-parameters frame)))))
        "=")))))
 
 ;;; Window sizing
