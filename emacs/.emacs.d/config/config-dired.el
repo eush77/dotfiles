e204f450c6e9caa344e9cb1690102d1ec7e9dfa6
Eugene Sharygin <eush77@gmail.com>
2018-10-24 09:30:25 +0300

    emacs/dired: add setup for find-file
    This enables jumping between project source and build directories.

--- emacs/.emacs.d/config/config-dired.el
+++ emacs/.emacs.d/config/config-dired.el
@@ -1,5 +1,7 @@
 (require 'dired-imenu)
 
+;;; Basic setup
+
 (custom-set dired-dwim-target t)
 (custom-set dired-listing-switches
             (concat "-lv --group-directories-first --human-readable"
@@ -7,6 +9,8 @@
 
 (add-hook 'dired-mode-hook #'dired-hide-details-mode)
 (add-hook 'dired-mode-hook #'dired-hide-dotfiles-mode)
+
+;;; Commands
 
 (defun my-dired-browse-file ()
   "Browse a file with `browse-url'."
@@ -29,6 +33,41 @@ the same window."
   (message "Dired DWIM target %s"
            (if dired-dwim-target "enabled" "disabled")))
 
+;;; Find-file
+
+;;;###autoload
+(defcustom my-src-directory "~/src"
+  "Directory with source files, with the structure mirroring
+`my-build-directory'."
+  :type 'directory
+  :group 'my)
+
+;;;###autoload
+(defcustom my-build-directory "~/build"
+  "Directory with build files, with the structure mirroring
+`my-src-directory'."
+  :type 'directory
+  :group 'my)
+
+(defun my-dired-ff-other-file (_)
+  "`ff-other-file-alist' function for Dired."
+  (cond ((file-in-directory-p default-directory my-build-directory)
+         (list (expand-file-name (file-relative-name default-directory
+                                                     my-build-directory)
+                                 my-src-directory)))
+        ((file-in-directory-p default-directory my-src-directory)
+         (list (expand-file-name (file-relative-name default-directory
+                                                     my-src-directory)
+                                 my-build-directory)))))
+
+(defun my-dired-ff-mode-hook ()
+  (setq ff-other-file-alist '(("\\.none" my-dired-ff-other-file))
+        ff-search-directories '("/")))
+
+(add-hook 'dired-mode-hook #'my-dired-ff-mode-hook)
+
+;;; Keymap
+
 (define-key dired-mode-map (kbd "C-M-p") #'window-jump-up)
 (define-key dired-mode-map (kbd "C-M-n") #'window-jump-down)
 
@@ -40,5 +79,9 @@ the same window."
 (define-key dired-mode-map (kbd "b") #'my-dired-browse-file)
 (define-key dired-mode-map (kbd "c") #'dired-kill-subdir)
 (define-key dired-mode-map (kbd "r") #'dired-do-query-replace-regexp)
+(define-key dired-mode-map (kbd "x") nil)
+(define-key dired-mode-map (kbd "X") #'dired-do-flagged-delete)
 (define-key dired-mode-map (kbd "z") #'dired-hide-subdir)
 (define-key dired-mode-map [mouse-2] #'my-dired-mouse-find-file)
+
+(key-chord-define dired-mode-map "xw" #'ff-get-other-file)
