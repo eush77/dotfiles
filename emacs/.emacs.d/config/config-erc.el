a53240f9fd7101971a6f6bf917b728f2e2cd34bf
Eugene Sharygin <eush77@gmail.com>
2019-02-16 10:14:03 +0300

    emacs/erc: update auth and module configs

--- emacs/.emacs.d/config/config-erc.el
+++ emacs/.emacs.d/config/config-erc.el
@@ -1,4 +1,36 @@
+;;; erc
+
+(custom-set erc-prompt-for-password nil)
+
+(add-to-list 'erc-modules 'log)
+
+;;; erc-log
+
+(with-eval-after-load "erc-log"
+  (custom-set erc-log-channels-directory
+              (expand-file-name "erc" user-emacs-directory)))
+
+;;; erc-pcomplete
+
+(defun my-pcomplete/erc-mode/complete-command ()
+  "Complete nicks first."
+  (pcomplete-here
+   (append
+    (pcomplete-erc-nicks erc-pcomplete-nick-postfix t)
+    (pcomplete-erc-commands))))
+
+(with-eval-after-load "erc-pcomplete"
+  (custom-set erc-pcomplete-nick-postfix ": ")
+  (custom-set erc-pcomplete-order-nickname-completions t)
+
+  (advice-add 'pcomplete/erc-mode/complete-command
+              :override #'my-pcomplete/erc-mode/complete-command))
+
+;;; erc-track
+
 (with-eval-after-load "erc-track"
+  (custom-set erc-track-enable-keybindings t)
+
   (add-to-list 'erc-track-exclude-types "JOIN")
   (add-to-list 'erc-track-exclude-types "PART")
   (add-to-list 'erc-track-exclude-types "QUIT")
