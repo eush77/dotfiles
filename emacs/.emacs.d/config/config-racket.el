6e67f5827c8802507a3f1e543b5738afffaf2c11
Eugene Sharygin <eush77@gmail.com>
2019-02-18 13:31:11 +0300

    emacs/racket: unbind C-c C-u in racket-mode-map
    This conflicts with global Smartparens key bindings.

--- emacs/.emacs.d/config/config-racket.el
+++ emacs/.emacs.d/config/config-racket.el
@@ -1,3 +1,4 @@
 (define-key racket-mode-map (kbd "C-c C-e") nil)
 (define-key racket-mode-map (kbd "C-c C-f") nil)
+(define-key racket-mode-map (kbd "C-c C-u") nil)
 (define-key racket-mode-map (kbd "C-M-y") nil)
