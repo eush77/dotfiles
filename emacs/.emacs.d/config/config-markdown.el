2d980ef3e69b246c350d6af54a2e8b0fe5862cb3
Eugene Sharygin <eush77@gmail.com>
2019-02-20 00:48:43 +0300

    emacs/markdown: split config into sections

--- emacs/.emacs.d/config/config-markdown.el
+++ emacs/.emacs.d/config/config-markdown.el
@@ -1,8 +1,15 @@
+;;; Auto-fill
+
+(add-hook 'markdown-mode-hook #'auto-fill-mode)
+
+;;; Keymap
+
 (define-key markdown-mode-map (kbd "M-<left>") #'markdown-promote)
 (define-key markdown-mode-map (kbd "M-<right>") #'markdown-demote)
 (define-key markdown-mode-map (kbd "M-<up>") #'markdown-move-up)
 (define-key markdown-mode-map (kbd "M-<down>") #'markdown-move-down)
-(add-hook 'markdown-mode-hook #'auto-fill-mode)
+
+;;; Smartparens
 
 (with-eval-after-load "smartparens"
   (sp-local-pair 'markdown-mode "`" "`"))
