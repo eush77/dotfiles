787ab54e2286526ac053bd71801940782c197d72
Eugene Sharygin <eush77@gmail.com>
2019-02-28 16:09:17 +0300

    emacs/dired: remap dired-diff key binding to ediff-files

--- emacs/.emacs.d/config/config-dired.el
+++ emacs/.emacs.d/config/config-dired.el
@@ -117,6 +117,7 @@ alternative names."
 (define-key dired-mode-map (kbd "C-M-n") #'window-jump-down)
 
 (define-key dired-mode-map (kbd ".") #'dired-hide-dotfiles-mode)
+(define-key dired-mode-map [remap dired-diff] #'ediff-files)
 (define-key dired-mode-map (kbd "SPC") #'dired-up-directory)
 (define-key dired-mode-map (kbd "C-x C-y") #'my-dired-toggle-dwim-target)
 (define-key dired-mode-map (kbd "M-p") #'dired-prev-subdir)
