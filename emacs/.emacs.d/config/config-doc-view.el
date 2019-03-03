caa3ced9bc9a57b8d61b708bb9c7c2db111a328c
Eugene Sharygin <eush77@gmail.com>
2019-03-03 10:22:26 +0300

    emacs/doc-view: don't ask questions on terminal displays
    This prevents the "View extracted text instead?" question from being
    asked on entry to `doc-view-mode`.

--- emacs/.emacs.d/config/config-doc-view.el
+++ emacs/.emacs.d/config/config-doc-view.el
@@ -1,4 +1,15 @@
+;;; Continuous Mode
+
 (custom-set doc-view-continuous t)
+
+;;; Terminal Display
+
+(when (and (not (display-graphic-p))
+           (executable-find doc-view-pdftotext-program))
+  (advice-add 'doc-view-initiate-display :override
+              #'doc-view-open-text))
+
+;;; Keymap
 
 (define-key doc-view-mode-map (kbd "k")
   #'doc-view-previous-line-or-previous-page)
