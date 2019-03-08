5a5d565984ddcd6ba9a54d2fd2154ef651727f09
Eugene Sharygin <eush77@gmail.com>
2019-03-08 08:08:23 +0300

    emacs/sgml: add command to browse-url current html file

--- emacs/.emacs.d/config/config-sgml.el
+++ emacs/.emacs.d/config/config-sgml.el
@@ -1,3 +1,15 @@
+;;; Commands
+
+(defun my-html-browse-file ()
+  (interactive)
+  "Load the current file in a web browser."
+  (if buffer-file-name
+      (browse-url buffer-file-name)
+    (user-error "Not in a file-visiting buffer")))
+
+;;; html-mode-map
+
+(define-key html-mode-map (kbd "C-c \\") #'my-html-browse-file)
 (define-key html-mode-map (kbd "C-c i") #'html-list-item)
 (define-key html-mode-map (kbd "C-c l") #'html-name-anchor)
 (define-key html-mode-map (kbd "C-c o") #'html-ordered-list)
