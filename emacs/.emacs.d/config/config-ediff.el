bb7a71cf8d1992d6a377e5c630a71f033a1ff771
Eugene Sharygin <eush77@gmail.com>
2019-03-21 08:39:01 +0300

    emacs/ediff: save window configuration around Ediff sessions
    This saves window configuration when entering Ediff, and restores it
    when quitting Ediff. Window configurations are saved per frame, so that
    configurations from simultaneous Ediff sessions do not interfere with
    each other.
    
    This replaces the previous use of `winner-undo` as a `ediff-quit-hook`
    which doesn't work when window configuration is changed during a Ediff
    sessions (with e.g. `ediff-toggle-split`).

--- emacs/.emacs.d/config/config-ediff.el
+++ emacs/.emacs.d/config/config-ediff.el
@@ -1,1 +1,28 @@
-(add-hook 'ediff-quit-hook #'winner-undo t)
+;;; -*- eval: (outline-minor-mode) -*-
+
+;;; Window Configuration
+
+(defvar my-ediff-startup-window-configurations nil
+  "Window configurations saved on entering Ediff.
+
+An alist (frame . wconf) of window configurations per frame.")
+
+(defun my-ediff-save-window-configuration ()
+  "Save to `my-ediff-startup-window-configurations'."
+  (setf (alist-get (selected-frame)
+                   my-ediff-startup-window-configurations)
+        (current-window-configuration)))
+(add-hook 'ediff-before-setup-hook
+          #'my-ediff-save-window-configuration)
+
+(defun my-ediff-restore-window-configuration ()
+  "Restore from `my-ediff-startup-window-configurations'."
+  (when-let ((window-configuration
+              (alist-get (selected-frame)
+                         my-ediff-startup-window-configurations)))
+    (set-window-configuration window-configuration)
+    (setf (alist-get (selected-frame)
+                     my-ediff-startup-window-configurations nil t)
+          nil)))
+(add-hook 'ediff-after-quit-hook-internal
+          #'my-ediff-restore-window-configuration)
