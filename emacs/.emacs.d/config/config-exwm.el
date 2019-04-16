76b5195e90ad76ed1befb80ce65c6a57cad58be9
Eugene Sharygin <eush77@gmail.com>
2019-04-16 16:51:09 +0300

    emacs/exwm: fix typos in XF86 key names

--- emacs/.emacs.d/config/config-exwm.el
+++ emacs/.emacs.d/config/config-exwm.el
@@ -39,9 +39,9 @@ See `my-exwm-brightness-down', `my-exwm-brightness-up'."
 
 (custom-set-variables
  '(exwm-input-global-keys
-   `((,(kbd "<XF86AudioLowerVolume") . emms-volume-lower)
-     (,(kbd "<XF86AudioMute") . my-emms-volume-mute)
-     (,(kbd "<XF86AudioRaiseVolume") . emms-volume-raise)
+   `((,(kbd "<XF86AudioLowerVolume>") . emms-volume-lower)
+     (,(kbd "<XF86AudioMute>") . my-emms-volume-mute)
+     (,(kbd "<XF86AudioRaiseVolume>") . emms-volume-raise)
      (,(kbd "<XF86MonBrightnessDown>") . my-exwm-brightness-down)
      (,(kbd "<XF86MonBrightnessUp>") . my-exwm-brightness-up)
      (,(kbd "<XF86TouchpadToggle>") . my-exwm-toggle-touchpad)
