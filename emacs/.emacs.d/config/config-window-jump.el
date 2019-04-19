c102624e8ce8ffe1b2de4a18a9f83aaa7a9eac0a
Eugene Sharygin <eush77@gmail.com>
2019-04-19 14:46:38 +0300

    emacs/window-jump: add support for Exwm
    This adds advice for `wj-all-windows` that filters to windows on active
    Exwm workspaces, which is the Exwm equivalent to filtering by virtual WM
    desktop.

--- emacs/.emacs.d/config/config-window-jump.el
+++ emacs/.emacs.d/config/config-window-jump.el
@@ -1,12 +1,12 @@
 (add-to-list 'package-selected-packages 'dash)
 (package-install-selected-packages)
+(require 'dash)
 
 (custom-set-variables '(wj-jump-frames (not (null window-system))))
 
 (defun my-wj-all-windows--selected-wm-desktop (windows)
   "Limit results to the selected virtual desktop of a window
 system."
-  (require 'dash)
   ;; Call `my-frame-wm-desktop' once per frame, not once per window.
   (let* ((wm-desktop-alist (--map (cons it (my-frame-wm-desktop it))
                                   (frame-list)))
@@ -15,5 +15,13 @@ system."
     (--filter (= (alist-get (window-frame it) wm-desktop-alist)
                  selected-wm-desktop)
               windows)))
+
+(defun my-wj-all-windows--exwm-active (windows)
+  "Limit to active Exwm workspaces."
+  (--filter (frame-parameter (window-frame it) 'exwm-active) windows))
+
 (advice-add 'wj-all-windows
-            :filter-return #'my-wj-all-windows--selected-wm-desktop)
+            :filter-return
+            (if (boundp 'exwm-state)
+                #'my-wj-all-windows--exwm-active
+              #'my-wj-all-windows--selected-wm-desktop))
