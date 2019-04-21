dafe4538439663cf14e1bbff25f429d9360bdf00
Eugene Sharygin <eush77@gmail.com>
2019-04-21 23:52:42 +0300

    emacs/pass: turn off view-mode
    The `view-mode` is for some reason turned on due to `view-read-only`
    being set to `t`

--- emacs/.emacs.d/config/config-pass.el
+++ emacs/.emacs.d/config/config-pass.el
@@ -1,1 +1,8 @@
-(define-key pass-mode-map (kbd "c") #'pass-copy)
+;;; view-mode
+
+(defun my-pass-setup-buffer--disable-view-mode ()
+  "Turn off `view-mode' toggled by `view-read-only' somehow."
+  (view-mode 0))
+
+(advice-add 'pass-setup-buffer
+            :after #'my-pass-setup-buffer--disable-view-mode)
