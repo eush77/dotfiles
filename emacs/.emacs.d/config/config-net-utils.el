e0207857bda4998f1725e02856093749c8ed8577
Eugene Sharygin <eush77@gmail.com>
2019-05-03 19:20:16 +0300

    emacs/net-utils: override ping command
    This defines command `my-ping` that overrides `ping` and
    
    - uses a separate host history;
    - uses last host if no host is found at point;
    - uses `net-utils-run-simple` to display the `ping` output;
    - selects the output window.

--- emacs/.emacs.d/config/config-net-utils.el
+++ emacs/.emacs.d/config/config-net-utils.el
@@ -1,9 +1,18 @@
-(defun my-ping--run-simple (func &rest args)
-  "Use `net-utils-run-simple'"
-  (cl-letf (((symbol-function 'net-utils-run-program)
-             (lambda (name _ program args)
-               (net-utils-run-simple (concat "*" name "*")
-                                     program
-                                     args))))
-    (apply func args)))
-(advice-add 'ping :around #'my-ping--run-simple)
+(defvar my-ping-history '("google.com")
+  "Host history for `my-ping'.")
+
+(defun my-ping (host)
+  "Ping HOST."
+  (interactive
+   (list (let ((machine-at-point (net-utils-machine-at-point)))
+           (read-string "Ping host: "
+                        (if (string-empty-p machine-at-point)
+                            (car my-ping-history)
+                          machine-at-point)
+                        'my-ping-history))))
+  (select-window
+   (net-utils-run-simple (concat "*Ping " host "*")
+                         ping-program
+                         (append ping-program-options (list host)))))
+
+(advice-add 'ping :override #'my-ping)
