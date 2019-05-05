28ae7a6330b645bf9f59f549c8d22a40953330c4
Eugene Sharygin <eush77@gmail.com>
2019-05-05 21:17:20 +0300

    emacs/mode-line: simplify buffer identification for dedicated windows
    This simplifies `sml/generate-buffer-identification` advice for
    dedicated windows using face remappings instead of modifying
    buffer-local value of `sml/buffer-identification`.

--- emacs/.emacs.d/config/config-mode-line.el
+++ emacs/.emacs.d/config/config-mode-line.el
@@ -40,31 +40,18 @@
  '(sml/size-indication-format "%p of %I ")
  '(sml/theme 'respectful))
 
-;;;###autoload
-(defface my-sml-dedicated-window-identification
-  '((t :inherit sml/filename :foreground "purple1"))
-  "Face used for buffer identification in dedicated windows."
-  :group 'my)
+(defvar-local my-sml-dedicated-window-identification-face-remap-cookie nil)
 
 (define-advice sml/generate-buffer-identification
     (:after (&rest _) my-dedicated-window)
   "Apply face `my-sml-dedicated-window-identification'."
-  (when-let ((face-begin
-              (or (text-property-any 0 (length sml/buffer-identification)
-                                     'face 'sml/filename
-                                     sml/buffer-identification)
-                  (text-property-any 0 (length sml/buffer-identification)
-                                     'face
-                                     'my-sml-dedicated-window-identification
-                                     sml/buffer-identification))))
-    (let ((face-end
-           (or (next-property-change face-begin sml/buffer-identification)
-               (length sml/buffer-identification))))
-      (put-text-property face-begin face-end
-                         'face (if (window-dedicated-p)
-                                   'my-sml-dedicated-window-identification
-                                 'sml/filename)
-                         sml/buffer-identification))))
+  (when my-sml-dedicated-window-identification-face-remap-cookie
+    (face-remap-remove-relative
+     my-sml-dedicated-window-identification-face-remap-cookie)
+    (setq my-sml-dedicated-window-identification-face-remap-cookie nil))
+  (when (window-dedicated-p)
+    (setq my-sml-dedicated-window-identification-face-remap-cookie
+          (face-remap-add-relative 'sml/filename :overline t :underline t))))
 
 (define-advice set-window-dedicated-p
     (:after (window _) sml/generate-buffer-identification)
