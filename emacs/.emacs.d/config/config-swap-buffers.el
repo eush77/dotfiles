bf7e45134a6549ffdb571159a950107fd643875c
Eugene Sharygin <eush77@gmail.com>
2019-05-08 15:03:34 +0300

    emacs/swap-buffers: refactor

--- emacs/.emacs.d/config/config-swap-buffers.el
+++ emacs/.emacs.d/config/config-swap-buffers.el
@@ -1,21 +1,26 @@
-(custom-set-variables '(swap-buffers-keep-focus t))
+;;; ASCII Art Labels
 
+(defun my-swap-buffers-asciiart-label (num)
+  "Replaces `swap-buffers-label' using `switch-window-asciiart'."
+  (nth (- num 1) switch-window-asciiart))
+
+;; Use ASCII art labels when on a text-only display to make the labels stand
+;; out.
 (unless (display-graphic-p)
   (add-to-list 'package-selected-packages 'switch-window)
   (package-install-selected-packages)
   (require 'switch-window-asciiart)
 
   (custom-set-variables
-   '(swap-buffers-qwerty-shortcuts
-     (progn (require 'switch-window-asciiart)
-            (mapcar (lambda (label)
-                      (let ((pos (1+ (string-match "(.)" label))))
-                        (substring label pos (1+ pos))))
-                    (butlast switch-window-asciiart)))))
+   `(swap-buffers-qwerty-shortcuts
+     ,(mapcar (lambda (label)
+                (let ((pos (1+ (string-match "(.)" label))))
+                  (substring label pos (1+ pos))))
+              (butlast switch-window-asciiart))))
 
-  (defun my-swap-buffers-label--asciiart (num)
-    "My replacement for `swap-buffers-label' using
-`switch-window-asciiart'."
-    (nth (- num 1) switch-window-asciiart))
   (advice-add 'swap-buffers-label
-              :override #'my-swap-buffers-label--asciiart))
+              :override #'my-swap-buffers-asciiart-label))
+
+;;; swap-buffers-keep-focus
+
+(custom-set-variables '(swap-buffers-keep-focus t))
