2a12681930baff93e8e43c6fe7869d207e3ecee5
Eugene Sharygin <eush77@gmail.com>
2019-05-21 02:37:53 +0300

    emacs/mailcap: add mime type for .svg

--- emacs/.emacs.d/config/config-mailcap.el
+++ emacs/.emacs.d/config/config-mailcap.el
@@ -1,2 +1,3 @@
 (add-to-list 'mailcap-mime-extensions '(".md" . "text/plain"))
 (add-to-list 'mailcap-mime-extensions '(".org" . "text/plain"))
+(add-to-list 'mailcap-mime-extensions '(".svg" . "image/svg+xml"))
