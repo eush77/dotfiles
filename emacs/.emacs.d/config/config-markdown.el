78d9dfdd4cf21a6e185fb7b292c9624329beb85e
Eugene Sharygin <eush77@gmail.com>
2019-08-10 20:22:52 +0300

    [emacs/markdown] Integrate my-nbsp-fix into fill-paragraph

--- emacs/.emacs.d/config/config-markdown.el
+++ emacs/.emacs.d/config/config-markdown.el
@@ -16,3 +16,22 @@
 
 (with-eval-after-load "smartparens"
   (sp-local-pair 'markdown-mode "`" "`"))
+
+;;; Typography
+
+(define-advice markdown-fill-paragraph
+    (:before (&rest _ignored) my-nbsp-fix)
+  "Fix non-breaking spacing."
+  (when-let ((nbsp (my-nbsp-get-sequence)))
+    (let ((begin (save-excursion
+                   (forward-char)
+                   (markdown-backward-paragraph)
+                   (point)))
+          (end (save-excursion
+                 (markdown-forward-paragraph)
+                 (point))))
+      (require 'guess-language)
+      (my-nbsp-fix (guess-language-region begin end)
+                   nbsp
+                   begin
+                   end))))
