d03e426ffa6b2c31cb03375285509e19cf5a83e6
Eugene Sharygin <eush77@gmail.com>
2019-11-05 12:15:11 +0300

    [emacs/rst] Fix nbsp automatically when filling paragraphs
    This hooks up `my-nbsp-fix-paragraph` into `fill-paragraph` via
    `fill-paragraph-function`.

--- emacs/.emacs.d/config/config-rst.el
+++ emacs/.emacs.d/config/config-rst.el
@@ -1,2 +1,7 @@
 (with-eval-after-load "smartparens"
   (sp-local-pair 'rst-mode "`" "`"))
+
+(cl-assert (null fill-paragraph-function))
+(setq-local fill-paragraph-function
+            (lambda (_)
+              (call-interactively 'my-nbsp-fix-paragraph)))
